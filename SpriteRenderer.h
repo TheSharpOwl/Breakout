f46284d47e40e6c2a9d3cf445a58493463685651
TheSharpOwl <ziadxkabakibi@hotmail.com>
2020-08-18 00:41:59 +0300

    added SpriteRender Class

---
+++ SpriteRenderer.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <glad/glad.h>
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+
+#include "Texture.h"
+#include "Shader.h"
+
+class SpriteRenderer
+{
+public:
+	SpriteRenderer(Shader& shader);
+	~SpriteRenderer();
+	void DrawSprite(Texture2D& texture, glm::vec2 position, glm::vec2 size = glm::vec2(10.0f, 10.0f), float rotate = 0.0f, glm::vec3 color = glm::vec3(1.0f));
+private:
+	//Render state
+	Shader shader;
+	unsigned int quadVAO;
+	void initRenderData();
+};
+
