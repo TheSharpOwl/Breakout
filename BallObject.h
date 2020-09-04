e2bac7c80a8bf9bd82f4874f7d753b9193ece69a
TheSharpOwl <ziadxkabakibi@hotmail.com>
2020-09-04 22:40:17 +0300

    so far seems good

---
+++ BallObject.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+
+#include "GameObject.h"
+#include "Texture.h"
+
+class BallObject : public GameObject
+{
+public:
+
+	float Radius;
+	bool Stuck;
+
+	BallObject();
+	BallObject(glm::vec2 pos, float radius, glm::vec2 velocity, Texture2D sprite);
+	// moves the ball, keeping it constrained within the window bounds (except bottom edge); returns new position
+	glm::vec2 move(float dt, unsigned int window_width);
+	// resets the ball to original state with given position and velocity
+	void Reset(glm::vec2 position, glm::vec2 velocity);
+};
+
